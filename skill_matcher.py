3e07435bd01ca4a0a62105303c6d1a2f07bd3e20
Deepak Prajapat <deepakprajapat7067@gmail.com>
2025-05-14 19:25:31 +0530

    Delete skill_matcher.py

--- skill_matcher.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def get_similarity_score(resume_text, jd_text):
-    vectorizer = TfidfVectorizer()
-    vectors = vectorizer.fit_transform([resume_text, jd_text])
-    score = cosine_similarity(vectors[0:1], vectors[1:2])[0][0]
-    return round(score * 100, 2)
